23b3079d0f6373c64a1318f98308eb0c4ede7a32
hritikchokker <hchokker7@gmail.com>
2021-06-24 09:56:28 +0530

    auth and posts library added"

---
+++ projects/post/src/lib/post.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-post',
+  template: `
+    <p>
+      post works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/post/src/lib/post.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PostComponent } from './post.component';
+
+
+
+@NgModule({
+  declarations: [
+    PostComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    PostComponent
+  ]
+})
+export class PostModule { }
